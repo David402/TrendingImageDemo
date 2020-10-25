76d87700e1dce636bfc662bac07ef778c65c644b
David Liu <shaowei.liu@gmail.com>
2020-10-25 16:23:54 -0700

    added new interface for performSearch with result size

--- app/src/main/java/com/github/david402/githubsearchdemo/SearchRepository.kt
+++ app/src/main/java/com/github/david402/githubsearchdemo/SearchRepository.kt
@@ -8,6 +8,7 @@ import java.util.stream.Collectors
 
 interface SearchApi {
     suspend fun performSearch(query: String): List<User>
+    suspend fun performSearch(query: String, size: Int): List<User>
 }
 
 class SearchRepository() : SearchApi {
@@ -18,9 +19,36 @@ class SearchRepository() : SearchApi {
     private val TAG = "Search Repository"
     private val githubService = GithubServiceBuilder.buildService(GithubServices::class.java)
 
-    override suspend fun performSearch(query: String): List<User> = withContext(Dispatchers.IO) {
-        val users = searchGithubUsers(query)
-        getUserInfo(users)
+    /**
+     * This function will perform Github search api for users matches `query` with Default result size
+     *
+     * Return - A list of `User` or empty list if no result matched
+     * Throw - NetworkErrorException when Github API Rate Limit hits
+     */
+    override suspend fun performSearch(query: String): List<User> {
+        return performSearch(query, DEFAULT_RESULT_MAX_SIZE)
+    }
+
+    inline fun measureTimeMillis(block: () -> Unit): Long {
+        val start = System.currentTimeMillis()
+        block()
+        return System.currentTimeMillis() - start
+    }
+
+    /**
+     * This function will perform Github search api for users matches `query` with custom result size
+     *
+     * Return - A list of `User` or empty list if no result matched
+     * Throw - NetworkErrorException when Github API Rate Limit hits
+     */
+    override suspend fun performSearch(query: String, size: Int): List<User> = withContext(Dispatchers.IO) {
+        val users = searchGithubUsers(query, size)
+        lateinit var result: List<User>
+        val elapsed = measureTimeMillis {
+            result = getUserInfo(users)
+        }
+        println("API call time elapsed: $elapsed")
+        result
     }
 
     private suspend fun searchGithubUsers(query: String, size: Int = DEFAULT_RESULT_MAX_SIZE): List<GithubUser> {
