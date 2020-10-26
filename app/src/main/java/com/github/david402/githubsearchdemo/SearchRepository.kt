64f0c3659a4f4f57debb7e2197ee5931c5fc819b
David Liu <shaowei.liu@gmail.com>
2020-10-25 20:47:38 -0700

    minor refactoring

--- app/src/main/java/com/github/david402/githubsearchdemo/SearchRepository.kt
+++ app/src/main/java/com/github/david402/githubsearchdemo/SearchRepository.kt
@@ -13,10 +13,10 @@ interface SearchApi {
 
 class SearchRepository() : SearchApi {
     companion object {
+        private const val TAG = "Search Repository"
         private const val DEFAULT_RESULT_MAX_SIZE = 10
     }
-
-    private val TAG = "Search Repository"
+    
     private val githubService = GithubServiceBuilder.buildService(GithubServices::class.java)
 
     /**
@@ -27,12 +27,6 @@ class SearchRepository() : SearchApi {
      */
     override suspend fun performSearch(query: String): List<User> {
         return performSearch(query, DEFAULT_RESULT_MAX_SIZE)
-    }
-
-    inline fun measureTimeMillis(block: () -> Unit): Long {
-        val start = System.currentTimeMillis()
-        block()
-        return System.currentTimeMillis() - start
     }
 
     /**
@@ -55,31 +49,34 @@ class SearchRepository() : SearchApi {
         Log.d(TAG, "search user - $query")
         return withContext(Dispatchers.IO) {
             val call = githubService.searchUsers(query, size)
-            Log.d(TAG,"API call - " + call.request().toString())
             val response = call.execute()
             if (!response.isSuccessful) {
                 Log.d(TAG,"API call failed")
                 Log.d(TAG, "code: " + response.code() + ", message: " + response.message())
                 throw NetworkErrorException("Github API Rate Limit Hits.")
             }
-            Log.d(TAG,"API call successful")
-            Log.d(TAG,"res.body() - ${response.body()}")
             val result = response.body()?.items
             result?: emptyList()
         }
     }
 
     private suspend fun getUserInfo(users: List<GithubUser>): List<User> = users.parallelStream().map { user ->
-            val call = githubService.getUserInfo(user.login)
-            val response = call.execute()
-            if (!response.isSuccessful) {
-                Log.d(TAG,"(2) API call failed - code=${response.code()}, message: ${response.message()}")
-                User(user.login, user.avatar_url, -1)
-            }
-            val result: UserInfoResult = response.body()!!
-            Log.d(TAG,"(2) API call successful")
-            Log.d(TAG,"(2) res.body() - ${response.body()}")
-            User(result?.login, result?.avatar_url, result?.public_repos)
-
+        val call = githubService.getUserInfo(user.login) // Retrofit call
+        val response = call.execute()
+        if (!response.isSuccessful) {
+            Log.d(
+                TAG,
+                "API call failed - code=${response.code()}, message: ${response.message()}"
+            )
+            throw NetworkErrorException("Github API Rate Limit Hits.")
+        }
+        val result: UserInfoResult = response.body()!!
+        User(result?.login, result?.avatar_url, result?.public_repos)
     }.collect(Collectors.toList())
+
+    inline fun measureTimeMillis(block: () -> Unit): Long {
+        val start = System.currentTimeMillis()
+        block()
+        return System.currentTimeMillis() - start
+    }
 }
